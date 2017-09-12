b47cb75717f8de7ac012ce1be15d1cca5c1c77b2
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-09-12 15:22:21 +0200

    inscription

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
 });
+
+Route::get('/inscription', function () {
+    return view('inscription');
+});
+
